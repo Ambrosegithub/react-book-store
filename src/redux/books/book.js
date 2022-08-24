c5c04c38b271391db774d1041457dce6720076ce
Isigba Ambrose <ambrisexyz@gmail.com>
2022-08-24 18:41:16 +0200

    makes changes in line with reviewer request

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,7 +1,38 @@
 // define constant
 const ADD_BOOK = 'bookstore/bookS/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/bookS/REMOVE_BOOK';
-const Books = [];
+const Books = [
+  {
+    id: 1,
+    title: 'Think Big',
+    author: 'Ben Carson',
+  },
+  {
+    id: 2,
+    title: 'Eloquent Javascript',
+    author: 'Marijn Haverbeke',
+  },
+  {
+    id: 3,
+    title: 'Power of Mind',
+    author: 'Pastor Chris Oyakhilomen PHD',
+  },
+  {
+    id: 4,
+    title: 'Thing Fall Apart',
+    author: 'Chinwe Achebe',
+  },
+  {
+    id: 5,
+    title: 'Treasure island',
+    author: 'Emily Bronte'
+  },
+  {
+    id: 6,
+    title: 'Reader are Leader',
+    author: 'BEN Murray'
+  },
+];
 const bookReducer = (state = Books, action) => {
   switch (action.type) {
     case ADD_BOOK:
